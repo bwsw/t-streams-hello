5aa1ea51743b78f7ee2b2e3d0713336560ff9913
bwsw <ivan.a.kudryavtsev@gmail.com>
2016-08-14 23:36:28 +0700

    Proper implementation.

--- build.sbt
+++ build.sbt
@@ -1,10 +1,10 @@
+val prodVersion = "1.0"
 name 		:= "t-streams-hello"
-version 	:= "1.0"
+version 	:= prodVersion
 scalaVersion 	:= "2.11.8"
-
 resolvers +=
 "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
 
-libraryDependencies ++= Seq("com.bwsw" % "t-streams_2.11" % "1.0-SNAPSHOT")
+//libraryDependencies ++= Seq("com.bwsw" % "t-streams_2.11" % "1.0A-SNAPSHOT")
 
-assemblyJarName in assembly := "ts-hello" + version + ".jar"
+assemblyJarName in assembly := "ts-hello" + prodVersion + ".jar"
--- src/main/scala-2.11/com/bwsw/t_sreams/hello/Main.scala
+++ src/main/scala-2.11/com/bwsw/t_sreams/hello/Main.scala
@@ -12,12 +12,12 @@ import com.bwsw.tstreams.converter.{StringToArrayByteConverter, ArrayByteToStrin
 /**
   * Created by ivan on 05.08.16.
   */
-object Hello {
+object HelloProducer {
   def main(args: Array[String]): Unit = {
 
     // create factory
     val f = new TStreamsFactory()
-    f.setProperty(TSF_Dictionary.Metadata.Cluster.NAMESPACE, "test").                 // keyspace must exist in C*
+    f.setProperty(TSF_Dictionary.Metadata.Cluster.NAMESPACE, "tk_1").                 // keyspace must exist in C*
       setProperty(TSF_Dictionary.Data.Cluster.NAMESPACE, "test").                     // exists by default in Aerospike
       setProperty(TSF_Dictionary.Producer.BIND_PORT, 18001).                          // producer will listen localhost:18001
       setProperty(TSF_Dictionary.Consumer.Subscriber.BIND_PORT, 18002).               // subscriber will listen localhost:18002
@@ -26,8 +26,8 @@ object Hello {
 
     val l = new CountDownLatch(1)
     var cntr = 0
-    val TOTAL_TXNS = 100
-    val TOTAL_ITMS = 100
+    val TOTAL_TXNS = 10000
+    val TOTAL_ITMS = 1
 
     // create producer
     val producer = f.getProducer[String](
@@ -37,39 +37,68 @@ object Hello {
                 partitions = List(0),                       // active partitions
                 isLowPriority = false)                      // agent can be a master
 
-
-    val subscriber = f.getSubscriber[String](
-                name          = "test_subscriber",              // name of the subscribing consumer
-                txnGenerator  = new LocalTimeUUIDGenerator,     // where it can get transaction uuids
-                converter     = new ArrayByteToStringConverter, // vice versa converter to string
-                partitions    = List(0),                        // active partitions
-                offset        = Newest,                         // it will start from newest available partitions
-                isUseLastOffset = false,                        // will ignore history
-                callback = new Callback[String] {
-                  override def onEvent(subscriber: SubscribingConsumer[String], partition: Int, transactionUuid: UUID): Unit = {
-                    val txn = subscriber.getTransactionById(partition, transactionUuid) // get transaction
-                    txn.get.getAll().foreach(i => i)                           // get all information from transaction
-                    cntr += 1
-                    if(cntr == TOTAL_TXNS)                                              // if the producer sent all information, then end
-                      l.countDown()
-                  }
-                })
-
-    subscriber.start() // start subscriber to operate
-
     val startTime = System.currentTimeMillis()
 
     (0 until TOTAL_TXNS).foreach(
       i => {
         val t = producer.newTransaction(policy = NewTransactionProducerPolicy.CheckpointIfOpened) // create new transaction
         (0 until TOTAL_ITMS).foreach(j => t.send(s"I: ${i}, J: ${j}"))
-        t.checkpoint()  // checkpoint the transaction
+        if (i % 100 == 0)
+          println(i)
+        t.checkpoint(false)  // checkpoint the transaction
       })
 
-    l.await()
     val stopTime = System.currentTimeMillis()
     println(s"Execution time is: ${stopTime - startTime}")
     producer.stop()   // stop operation
+    f.close()         // end operation
+    System.exit(0)
+  }
+}
+
+/**
+  * Created by ivan on 05.08.16.
+  */
+object HelloSubscriber {
+  def main(args: Array[String]): Unit = {
+
+    // create factory
+    val f = new TStreamsFactory()
+    f.setProperty(TSF_Dictionary.Metadata.Cluster.NAMESPACE, "tk_1").                 // keyspace must exist in C*
+      setProperty(TSF_Dictionary.Data.Cluster.NAMESPACE, "test").                     // exists by default in Aerospike
+      setProperty(TSF_Dictionary.Producer.BIND_PORT, 18001).                          // producer will listen localhost:18001
+      setProperty(TSF_Dictionary.Consumer.Subscriber.BIND_PORT, 18002).               // subscriber will listen localhost:18002
+      setProperty(TSF_Dictionary.Consumer.Subscriber.PERSISTENT_QUEUE_PATH, "/tmp").  // subscriber will store data bursts in /tmp
+      setProperty(TSF_Dictionary.Stream.NAME, "test-stream")                          // producer and consumer will operate on "test-stream" t-stream
+
+    val l = new CountDownLatch(1)
+    var cntr = 0
+    val TOTAL_TXNS = 10000
+
+    val subscriber = f.getSubscriber[String](
+      name          = "test_subscriber",              // name of the subscribing consumer
+      txnGenerator  = new LocalTimeUUIDGenerator,     // where it can get transaction uuids
+      converter     = new ArrayByteToStringConverter, // vice versa converter to string
+      partitions    = List(0),                        // active partitions
+      offset        = Newest,                         // it will start from newest available partitions
+      isUseLastOffset = false,                        // will ignore history
+      callback = new Callback[String] {
+        override def onEvent(subscriber: SubscribingConsumer[String], partition: Int, transactionUuid: UUID): Unit = {
+          val txn = subscriber.getTransactionById(partition, transactionUuid) // get transaction
+          txn.get.getAll().foreach(i => i)                           // get all information from transaction
+          cntr += 1
+          if (cntr % 100 == 0)
+            println(cntr)
+          if(cntr == TOTAL_TXNS)                                              // if the producer sent all information, then end
+            l.countDown()
+        }
+      })
+
+    subscriber.start() // start subscriber to operate
+
+    val startTime = System.currentTimeMillis()
+
+    l.await()
     subscriber.stop() // stop operation
     f.close()         // end operation
     System.exit(0)
